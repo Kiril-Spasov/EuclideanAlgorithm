3bf665c9559a09c1ef032b9a40c68e5529d2ec3c
Kiril Spasov <spasov.kiril@gmail.com>
2022-12-14 12:16:07 +0200

    Add project files.

---
+++ EuclideanAlgorithm/FormEuclideanAlgorithm.Designer.cs
@@ -0,0 +1,99 @@
+﻿namespace EuclideanAlgorithm
+{
+    partial class FormEuclideanAlgorithm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.BtnFindGCD = new System.Windows.Forms.Button();
+            this.TxtResult = new System.Windows.Forms.TextBox();
+            this.BtnGcdOf3Numbers = new System.Windows.Forms.Button();
+            this.BtnFindLCM = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // BtnFindGCD
+            // 
+            this.BtnFindGCD.Location = new System.Drawing.Point(42, 52);
+            this.BtnFindGCD.Name = "BtnFindGCD";
+            this.BtnFindGCD.Size = new System.Drawing.Size(162, 57);
+            this.BtnFindGCD.TabIndex = 0;
+            this.BtnFindGCD.Text = "Find the GCD";
+            this.BtnFindGCD.UseVisualStyleBackColor = true;
+            this.BtnFindGCD.Click += new System.EventHandler(this.BtnFindGCD_Click);
+            // 
+            // TxtResult
+            // 
+            this.TxtResult.Location = new System.Drawing.Point(234, 52);
+            this.TxtResult.Multiline = true;
+            this.TxtResult.Name = "TxtResult";
+            this.TxtResult.Size = new System.Drawing.Size(452, 342);
+            this.TxtResult.TabIndex = 1;
+            // 
+            // BtnGcdOf3Numbers
+            // 
+            this.BtnGcdOf3Numbers.Location = new System.Drawing.Point(42, 140);
+            this.BtnGcdOf3Numbers.Name = "BtnGcdOf3Numbers";
+            this.BtnGcdOf3Numbers.Size = new System.Drawing.Size(161, 59);
+            this.BtnGcdOf3Numbers.TabIndex = 2;
+            this.BtnGcdOf3Numbers.Text = "Find GCD of 3 numbers";
+            this.BtnGcdOf3Numbers.UseVisualStyleBackColor = true;
+            this.BtnGcdOf3Numbers.Click += new System.EventHandler(this.BtnGcdOf3Numbers_Click);
+            // 
+            // BtnFindLCM
+            // 
+            this.BtnFindLCM.Location = new System.Drawing.Point(42, 232);
+            this.BtnFindLCM.Name = "BtnFindLCM";
+            this.BtnFindLCM.Size = new System.Drawing.Size(160, 60);
+            this.BtnFindLCM.TabIndex = 3;
+            this.BtnFindLCM.Text = "Find the LCM";
+            this.BtnFindLCM.UseVisualStyleBackColor = true;
+            this.BtnFindLCM.Click += new System.EventHandler(this.BtnFindLCM_Click);
+            // 
+            // FormEuclideanAlgorithm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.BtnFindLCM);
+            this.Controls.Add(this.BtnGcdOf3Numbers);
+            this.Controls.Add(this.TxtResult);
+            this.Controls.Add(this.BtnFindGCD);
+            this.Name = "FormEuclideanAlgorithm";
+            this.Text = "Euclidean Algorithm";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button BtnFindGCD;
+        private System.Windows.Forms.TextBox TxtResult;
+        private System.Windows.Forms.Button BtnGcdOf3Numbers;
+        private System.Windows.Forms.Button BtnFindLCM;
+    }
+}
+
---
+++ EuclideanAlgorithm/FormEuclideanAlgorithm.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace EuclideanAlgorithm
+{
+    public partial class FormEuclideanAlgorithm : Form
+    {
+        public FormEuclideanAlgorithm()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnFindGCD_Click(object sender, EventArgs e)
+        {
+            int number1 = Convert.ToInt32(Microsoft.VisualBasic.Interaction.InputBox("Enter first number: ", "GCD"));
+            int number2 = Convert.ToInt32(Microsoft.VisualBasic.Interaction.InputBox("Enter second number: ", "GCD"));
+
+            TxtResult.Text = "The GCD is " + FindGCD(number1, number2);
+        }
+
+        //"A more efficient method is the Euclidean algorithm,
+        //a variant in which the difference of the two numbers a and b is replaced by the remainder of the Euclidean division (also called division with remainder) of a by b.
+        //Denoting this remainder as a mod b, the algorithm replaces (a, b) by (b, a mod b) repeatedly until the pair is (d, 0),
+        //where d is the greatest common divisor."
+        //https://en.wikipedia.org/wiki/Greatest_common_divisor
+        private int FindGCD(int n1, int n2)
+        {
+            int remainder;
+            int result = 0;
+
+            int number1 = Math.Abs(n1);
+            int number2 = Math.Abs(n2);
+
+            while (number1 != 0 && number2 != 0)
+            {
+                if (number1 > number2)
+                {
+                    remainder = number1 % number2;
+                    number1 = remainder;
+                }
+                else
+                {
+                    remainder = number2 % number1;
+                    number2 = remainder;
+                }
+
+                if (remainder != 0)
+                {
+                    result = remainder;
+                }
+            }
+            return result;
+        }
+
+        //Another implementation of the Euclidean algorithm.
+        private int FindGCD2(int n1, int n2)
+        {
+            int remainder = 0;
+            int temp = 0;
+
+            int number1 = Math.Abs(n1);
+            int number2 = Math.Abs(n2);
+
+            if (number1 < number2)
+            {
+                number1 = temp;
+                number1 = number2;
+                number2 = temp;
+            }
+
+            while (true)
+            {
+                remainder = number1 % number2;
+
+                if (remainder == 0)
+                    break;
+
+                number1 = number2;
+                number2 = remainder;
+            }
+
+            return number2;
+        }
+
+        private void BtnGcdOf3Numbers_Click(object sender, EventArgs e)
+        {
+            int number1 = Convert.ToInt32(Microsoft.VisualBasic.Interaction.InputBox("Enter first number: ", "GCD"));
+            int number2 = Convert.ToInt32(Microsoft.VisualBasic.Interaction.InputBox("Enter second number: ", "GCD"));
+            int number3 = Convert.ToInt32(Microsoft.VisualBasic.Interaction.InputBox("Enter third number: ", "GCD"));
+
+            TxtResult.Text = "The GCD is " + FindGCD(number1, FindGCD(number2, number3));
+        }
+
+        private void BtnFindLCM_Click(object sender, EventArgs e)
+        {
+            int number1 = Convert.ToInt32(Microsoft.VisualBasic.Interaction.InputBox("Enter first number: ", "LCD"));
+            int number2 = Convert.ToInt32(Microsoft.VisualBasic.Interaction.InputBox("Enter second number: ", "LCD"));
+
+            TxtResult.Text = "The LCD is " + FindLCD(number1, number2);
+        }
+
+        //Finding the lowest common multiple using gcd. 
+        private int FindLCD(int number1, int number2)
+        {
+            int lcd = number1 * number2 / FindGCD(number1, number2);
+
+            return lcd;
+        }
+    }
+}
